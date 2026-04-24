9b57a60cbc2f5cd7bf12fefb6a9b880e3e821cfe
lanibread <brendikali@gmail.com>
2026-04-23 21:45:10 -0400

    Changed color

--- App.js
+++ App.js
@@ -33,7 +33,7 @@ export default function App() {
       title={
         <Text style={
           item.completed
-            ? { textDecorationLine: 'line-through', textDecorationStyle: 'solid', marginLeft: 10, color: '#D5F1FF' }
+            ? { textDecorationLine: 'line-through', textDecorationStyle: 'solid', marginLeft: 10, color: '#F1D5FF' }
             : { marginLeft: 10 }
         }>
           {item.description}
