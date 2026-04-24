9b468d8d31a4ab79d8b022139b9a6dd93b1f784a
lanibread <brendikali@gmail.com>
2026-04-23 20:51:51 -0400

    added components

--- App.js
+++ App.js
@@ -3,11 +3,17 @@ import { StyleSheet, View, FlatList, SafeAreaView } from 'react-native';
 import { CheckBox, Input, Button, Text } from '@rneui/themed';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+  const [tasks, setTasks] = useState([
+    { key: '1', description: 'Learn React Native', completed: false},
+    { key: '2', description: 'Master FlatList', completed: true},
+  ]);
+
+  const [inputText, setInputText] = useState('');
+
+  const addTask = () => {
+    if (inputText.trim().length > 0) {
+      const newTask = {
+        key: Math.random().toString(),
   );
 }
 
